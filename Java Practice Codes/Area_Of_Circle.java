d51928554da57314c8b233c17cf1cb2dc0555679
KaizenPrad <namandwivedi32@gmail.com>
2025-01-24 09:25:08 +0530

    Area_of_Cirlce

---
+++ Java Practice Codes/Area_Of_Circle.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Area_Of_Circle {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the Radius of Circle : ");
+            float radius = sc.nextFloat();
+            float area = ((radius * radius ) * 22/7);
+            System.out.println("The Area of Circle : " + area);
+            sc.close();
+                }
+}
