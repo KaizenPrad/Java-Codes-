ab763050803f36b1499490655901b62987901828
KaizenPrad <namandwivedi32@gmail.com>
2025-01-27 14:29:06 +0530

    Name change

--- CommissionPrecentage.java
+++ CommissionPrecentage.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; 
 
-public class sumofNnumber {
+public class CommissionPrecentage {
      public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
             System.out.print("Enter the Total Sale Amount : ");
