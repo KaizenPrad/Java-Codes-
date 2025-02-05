0e7b34489c515ef3c54e1ac94dfeaaeb936d89a2
KaizenPrad <namandwivedi32@gmail.com>
2025-02-05 17:23:59 +0530

    Enhacning once again

--- Java Practice Codes/nCrAndpCr_Code.java
+++ Java Practice Codes/nCrAndpCr_Code.java
@@ -4,6 +4,8 @@ public class nCrAndpCr_Code {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Choose 1. nCr " + "\n Choose 2. nPr : ");
+        int choice = sc.nextInt();
         // Input for n and r
         System.out.println("Enter the {N} Value for nCr Formula: ");
         int n = sc.nextInt();
@@ -11,17 +13,33 @@ public class nCrAndpCr_Code {
         System.out.println("Enter the {R} Value for nCr Formula: ");
         int r = sc.nextInt();
 
-        // Calculate nCr using the formula: nCr = n! / (r! * (n - r)!)
-        int result = f(n) / (f(r) * f(n - r));
-        if (result!=0) {
-            System.out.println("nCr = " + result);
-            
-        }
-        else{
-            System.out.println("You have Entered Wrong Value keep N Value Greater than R!!!!");
+        if (choice == 1) {
+            FirstNCRCalculator(n, r);
+        } else if (choice == 2) {
+            FirstNPRCalculator(n, r);
         }
 
         sc.close();
+    }
+
+    static void FirstNPRCalculator(int n, int r) {
+        int result = f(n) / f(n - r);
+        if (result != 0) {
+            System.out.println("nCr = " + result);
+
+        } else {
+            System.out.println("You have Entered Wrong Value keep N Value Greater than R!!!!");
+        }
+    }
+
+    static void FirstNCRCalculator(int n, int r) {
+        int result = f(n) / (f(r) * f(n - r));
+        if (result != 0) {
+            System.out.println("nCr = " + result);
+
+        } else {
+            System.out.println("You have Entered Wrong Value keep N Value Greater than R!!!!");
+        }
     }
 
     // Factorial function
